ec79f714057da3338b2e1c090cf4854e9f954137
Alex Yong <alexjyong@gmail.com>
2023-01-07 18:39:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ const client = new plaid.Client({
   secret: secret,
   env: env
 });
+
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+const startDate = yesterday.toISOString().slice(0, 10);
+const endDate = 'today';
+
+client.transactions.get(access_token, startDate, endDate, (error, transactionsResponse) => {
+  if (error != null) {
+    console.log(error);
+  } else {
+    console.log(transactionsResponse.transactions);
+  }
+});
